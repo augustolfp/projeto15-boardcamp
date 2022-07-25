df71ebc8a1611f4d0239e97bc9b7c43bfb92b49b
Augusto Lopes <augustolfp@gmail.com>
2022-07-25 14:20:54 -0300

    Implements PUT /customers/:id endpoint to update data

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -43,3 +43,15 @@ export async function insertCustomer(req, res) {
         return res.send(error).status(401);
     }
 }
+
+export async function updateCustomer(req, res) {
+    const {name, phone, cpf, birthday} = req.body;
+    const id = parseInt(req.params.id);
+    try {
+        const query = await connection.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`,[name,phone,cpf,birthday,id]);
+        return res.sendStatus(200);
+    }
+    catch(error) {
+        return res.send(error);
+    }
+}
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { listCustomers, insertCustomer, findCustomer } from "../controllers/customersController.js";
+import { listCustomers, insertCustomer, findCustomer, updateCustomer } from "../controllers/customersController.js";
 import validateCustomer from '../middlewares/insertCustomerCheckMiddleware.js';
 const customersRouter = Router();
 
 customersRouter.get('/customers', listCustomers);
 customersRouter.post('/customers', validateCustomer, insertCustomer);
 customersRouter.get('/customers/:id', findCustomer);
+customersRouter.put('/customers/:id', validateCustomer, updateCustomer);
 
 export default customersRouter;
