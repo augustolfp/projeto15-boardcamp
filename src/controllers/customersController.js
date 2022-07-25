e17586daddc1c608f19dacc8e1b48ab3804b2141
Augusto Lopes <augustolfp@gmail.com>
2022-07-25 11:37:08 -0300

    Adds pattern matching with query string on GET customers endpoint

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,9 +1,16 @@
 import connection from "../database/database.js";
 
 export async function listCustomers(req, res) {
+    const cpf = req.query.cpf;
     try {
-        const query = await connection.query('SELECT * FROM customers');
-        return res.send(query.rows).status(200);
+        if(cpf) {
+            const query = await connection.query(`SELECT * FROM customers WHERE cpf LIKE $1`,[cpf+"%"]);
+            return res.send(query.rows).status(200);
+        }
+        else {
+            const query = await connection.query('SELECT * FROM customers');
+            return res.send(query.rows).status(200);
+        }
     }
     catch(error) {
         return res.send(error).status(400);
