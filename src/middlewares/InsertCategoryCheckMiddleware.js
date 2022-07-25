af84830c9887c7017e5ecd8a2f7ccab66cb165b9
Augusto Lopes <augustolfp@gmail.com>
2022-07-24 22:48:27 -0300

    Implements schema to check if POST category name is empty

--- src/middlewares/InsertCategoryCheckMiddleware.js
+++ src/middlewares/InsertCategoryCheckMiddleware.js
@@ -1,7 +1,15 @@
 import connection from "../database/database.js";
+import {categorySchema} from '../schemas/categorySchema.js';
 
 export default async function validateCategory(req, res, next) {
     const {name} = req.body;
+    const validation = categorySchema.validate({name});
+
+    if(validation.error) {
+        console.log(validation.error.details);
+        return res.sendStatus(400);
+    }
+    
     try {
         const categoryAlreadyExists = await connection.query(`SELECT EXISTS (SELECT 1 FROM categories WHERE name = $1)`,[name]);
         if(categoryAlreadyExists.rows[0].exists) {
